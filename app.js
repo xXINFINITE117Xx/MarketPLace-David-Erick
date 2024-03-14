65c7e64f33614ac902be7d1f0fb5cf00a83a8fdd
David Gaona <davidgaonahenao@gmail.com>
2024-03-14 13:23:37 -0500

    New function from the JS and also new modifications to the HTML and a little modification for the desccription from the products cards

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuHamburguer = document.querySelector('.menu-hamburguer');
-    const menu = document.querySelector('.menu');
-  
-    menuHamburguer.addEventListener('click', function () {
-      if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-      } else {
-        menu.style.display = 'block';
-      }
-    });
+window.addEventListener("DOMContentLoaded", getAPI);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuHamburguer = document.querySelector(".menu-hamburguer");
+  const menu = document.querySelector(".menu");
+
+  menuHamburguer.addEventListener("click", function () {
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
   });
-  
-  function changePage(page) {
-    alert(`Redirecting to ${page} page`);
-    // Puedes redirigir a la página correspondiente o realizar cualquier otra acción aquí
-    closeMenu();
-  }
-  
-  function closeMenu() {
-    const menu = document.querySelector('.menu');
-    menu.style.display = 'none';
-  }
-  
-  
+});
+
+function changePage(page) {
+  alert(`Redirecting to ${page} page`);
+  closeMenu();
+}
+
+function closeMenu() {
+  const menu = document.querySelector(".menu");
+  menu.style.display = "none";
+}
+
+function getAPI() {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then(data => {
+      data.forEach(product => createProductCard(product));
+    })
+    .catch(error => console.error('Error fetching API:', error));
+}
+
+getAPI();
+
+function createProductCard(productData) {
+  const container = document.createElement('div');
+  container.classList.add('container_product');
+
+  const img = document.createElement('img');
+  img.src = productData.image;
+  img.alt = productData.title;
+  container.appendChild(img);
+
+  const title = document.createElement('h2');
+  title.classList.add('titles');
+  title.textContent = productData.title;
+  container.appendChild(title);
+  const cardsContainer = document.querySelector('.container-cards');
+  cardsContainer.appendChild(container);
+}
+
+
+getAPI();
