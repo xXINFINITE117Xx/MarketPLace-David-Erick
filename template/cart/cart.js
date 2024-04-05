4aa29fd0cecb6c4bf9c7384b4590c0c4b58f9536
ErickArias <eariascardona0@soy.sena.edu.co>
2024-04-05 07:48:24 -0500

    funcionalidad completa carrito de compras

--- template/cart/cart.js
+++ template/cart/cart.js
@@ -1,66 +1,86 @@
 showProducts()
 
 function showProducts() {
-    let cart = JSON.parse(localStorage.getItem('cart'))
-    cart.forEach(productData => createProductElement(productData))
+    let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'))
+    shoppingCart.forEach(product => createProductElement(product))
 }
 
 
-const productsContainer = document.querySelector(".container_products");
-function createProductElement(productData) {
+function createProductElement(product) {
+    const productsContainer = document.querySelector(".container_products");
     //contenedor de los productos
     let quantityProduct = 0
     
     //contenedor del producto
-    const container_card = document.querySelector(".container_card");
+    const container_card = document.createElement("div");
+    container_card.classList.add("container_card");
     
-    const img = document.querySelector(".img");
-    img.src = productData.image;
+    const img = document.createElement("img");
+    img.src = product.image;
     
-    const description = document.querySelector(".description");
+    const description = document.createElement("div");
+    description.classList.add("description");
     
-    const title = document.querySelector(".title");
-    title.textContent = productData.title;
-    
-    const price = document.querySelector(".price");
-    price.textContent = "$" + productData.price;
+
+    const title = document.createElement("h3");
+    title.classList.add("title");
+    title.textContent = product.title;
+
+
+    const price = document.createElement("h3");
+    price.classList.add("price");
+    price.textContent = "$" + product.price;
     
     description.appendChild(title);
     description.appendChild(price);
     
     
-    const amount = document.querySelector(".amount");
-    const increase = document.querySelector(".increase");
-    const number_amount = document.querySelector(".number_amount");
+    const amount = document.createElement("div");
+    amount.classList.add("amount");
+
+    const increase = document.createElement("button");
+    increase.classList.add("increase");
+    increase.textContent = "+" ;
+
+
+    const number_amount = document.createElement("h4");
+    number_amount.classList.add("number_amount");
     number_amount.textContent = quantityProduct
     
-    const decrease = document.querySelector(".decrease");
-    
+    const decrease = document.createElement("button");
+    decrease.classList.add("decrease");
+    decrease.textContent = "-" ;
+
     amount.appendChild(increase);
     amount.appendChild(number_amount);
     amount.appendChild(decrease);
-    
+
+
+    container_card.appendChild(img);
+    container_card.appendChild(description);
+    container_card.appendChild(amount);
+
+    productsContainer.appendChild(container_card);
     
     // boton decrement
     
     decrease.addEventListener('click', () => {
 
-        if (productData.number_amount > 0) {
-            productData.number_amount--
+        if (product.number_amount > 0) {
+            product.number_amount--
         }
         
-        quantityProduct = productData.number_amount >= 0 ? productData.number_amount : 0
+        quantityProduct = product.number_amount >= 0 ? product.number_amount : 0
         number_amount.textContent = quantityProduct
         
     })
     
     increase.addEventListener('click', () => {
         
-        productData.number_amount++
-        quantityProduct = productData.number_amount
+        product.number_amount++
+        quantityProduct = product.number_amount
         number_amount.textContent = quantityProduct
-        
     })
-    productsContainer.appendChild(container_card);
+        
     
 }
